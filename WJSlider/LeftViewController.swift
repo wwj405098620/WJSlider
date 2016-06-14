c19f4d1ba917aba2880ff290a5f94d7660664069
wwj405098620 <wwj405098620@126.com>
2016-06-14 10:39:29 +0800

    1.0
    first version

---
+++ WJSlider/LeftViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LeftViewController.swift
+//  WJSlider
+//
+//  Created by 王文杰 on 16/6/13.
+//  Copyright © 2016年 王文杰. All rights reserved.
+//
+
+import UIKit
+
+class LeftViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WJSlider/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  WJSlider
+//
+//  Created by 王文杰 on 16/6/13.
+//  Copyright © 2016年 王文杰. All rights reserved.
+//
+
+import UIKit
+
+
+
+class BaseNavigationController: UINavigationController,WJLeftSlider{
+}
+
+class ViewController: UIViewController{
+    @IBAction func openLeft(sender: AnyObject) {
+        let nav = self.navigationController as! BaseNavigationController
+        nav.openLeftSliderController()
+    }
+    @IBAction func closeLeft(sender: AnyObject) {
+        let nav = self.navigationController as! BaseNavigationController
+        nav.closeLeftSliderController()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blueColor()
+        let leftController = LeftViewController()
+        var nav = self.navigationController as! BaseNavigationController
+        nav.setUpLeftSlider(leftController)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+
